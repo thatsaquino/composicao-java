562193a45f81d0f1c1d053bf7f5541ac14150914
thaqnos <thaisaquino1994@gmai.com>
2021-08-24 19:20:42 -0300

    Cria entidade de HoraContrato

---
+++ src/entidade/HoraContrato.java
@@ -0,0 +1,43 @@
+package entidade;
+
+import java.util.Date;
+
+public class HoraContrato {
+	
+	private Date data;
+	private Double valorPorHora;
+	private Integer hora;
+	
+	public HoraContrato() {}
+
+	public HoraContrato(Date data, Double valorPorHora, Integer hora) {
+		this.data = data;
+		this.valorPorHora = valorPorHora;
+		this.hora = hora;
+	}
+
+	public Date getData() {
+		return data;
+	}
+
+	public void setData(Date data) {
+		this.data = data;
+	}
+
+	public Double getValorPorHora() {
+		return valorPorHora;
+	}
+
+	public void setValorPorHora(Double valorPorHora) {
+		this.valorPorHora = valorPorHora;
+	}
+
+	public Integer getHora() {
+		return hora;
+	}
+
+	public void setHora(Integer hora) {
+		this.hora = hora;
+	}
+	
+}
