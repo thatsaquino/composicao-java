40e9be8506cf29d6489428daddb7009a9ac7af80
thaqnos <thaisaquino1994@gmai.com>
2021-08-25 19:20:50 -0300

    Adiciona contratos no main e instancia classe Trabalhador

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,18 +1,23 @@
 package aplicacao;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Locale;
 import java.util.Scanner;
 
 import entidade.Departamento;
+import entidade.HoraContrato;
 import entidade.Trabalhador;
 import entidade.enums.Senioridade;
 
 public class Programa {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
 		
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		
 		System.out.print("Entre com o nome do Departamento: ");
 		String nomeDepartamento = sc.nextLine();
@@ -31,7 +36,28 @@ public class Programa {
 									Senioridade.valueOf(nivel), //pega enum e recebe dado digitado comparando com variavel constante
 									salarioBase, //pega valor digitado
 									new Departamento(nomeDepartamento)); //instancia o objeto departamento e aplica como argumento o que foi digitado
-
+		
+		System.out.println("Quantos contratos esse trabalhador tem? ");
+		int n = sc.nextInt();
+		
+		for(int i=1; i<=n; i++) {
+			System.out.println("Entre com o contrato #" + i);
+			
+			System.out.print("Data (DD/MM/AAAA");
+			Date dataContrato = sdf.parse(sc.next());
+			
+			System.out.print("Valor por hora: ");
+			Double valorHora = sc.nextDouble();
+			
+			System.out.print("Duração (horas): ");
+			int horas = sc.nextInt();
+			
+			HoraContrato contrato = new HoraContrato(dataContrato, valorHora, horas);
+			
+			trabalhador.adicionaContrato(contrato);			
+		}
+		
+		
 		sc.close();
 
 	}
